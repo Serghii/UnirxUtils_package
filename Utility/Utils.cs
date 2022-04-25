b4b3bdd017cdd7cb90a25d669709d4ac1a810458
Serhii <s.safonov@room8studio.com>
2022-04-25 14:35:42 +0300

    init

---
+++ Utility/Utils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using TMPro;
+using Zenject;
+
+namespace DotweenStuff
+{
+    public static class Utils
+    {
+        public static  T GetFromSceneContext<T>() where T : class 
+        {
+            var source = ProjectContext.Instance.Container.Resolve<SceneContextRegistry>().SceneContexts;
+            if (source is IList<SceneContext> sourceList)
+            {
+                if (sourceList.Count > 0)
+                {
+                    return sourceList[0]?.Container?.Resolve<T>();
+                }
+                return null;
+            }
+            else
+            {
+                using (IEnumerator<SceneContext> enumerator = source.GetEnumerator())
+                {
+                    if (!enumerator.MoveNext())
+                        return null;
+                    SceneContext current = enumerator.Current;
+                    if (!enumerator.MoveNext())
+                        return current.Container.Resolve<T>();;
+                }
+            }
+            return null;
+        }
+        
+        public static IDisposable SubscribeToText(this System.IObservable<string> source, TextMeshProUGUI text)
+        {
+            return source.SubscribeWithState(text, (x, t) => t.text = x);
+        }
+        
+        public static IDisposable SubscribeToText<T>(this System.IObservable<T> source, TextMeshProUGUI text)
+        {
+            return source.SubscribeWithState(text, (x, t) => t.text = x.ToString());
+        }
+
+    }
+}
